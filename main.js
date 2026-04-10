b24bbf10bbc6b09e76e4aee35fed235e4bec611f
natconley <natalieannaconley@gmail.com>
2026-04-10 11:56:20 +0200

    New layout, new nav menu

---
+++ main.js
@@ -0,0 +1,18 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+const icon = menuToggle.querySelector('.material.icons');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+    if (nav.classList.contains('active')) {
+        icon.textContent = 'close';
+    } else {
+        icon.textContent = 'menu';
+    }
+});
+
+menuToggle.setAttribute(
+    'aria-expanded',
+    nav.classList.contains('active')
+);
