37cbedf80ce239952755a53e638a95d8f5bfcd29
natconley <natalieannaconley@gmail.com>
2026-05-21 10:25:25 +0200

    edits to layout and styling

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav');
 const icon = menuToggle.querySelector('.material-icons');
 
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+
+// menu toggle
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 
@@ -16,3 +20,20 @@ menuToggle.setAttribute(
     'aria-expanded',
     nav.classList.contains('active')
 );
+
+// lightbox - gallery
+document.querySelectorAll('.gallery-img').forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightboxImg.alt = img.alt;
+        lightbox.classList.add('active');
+    });
+});
+
+document.querySelector('.lightbox-close').addEventListener('click', () => {
+    lightbox.classList.remove('active');
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) lightbox.classList.remove('active');
+});
